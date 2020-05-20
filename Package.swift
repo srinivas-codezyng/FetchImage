c6eeaf41de260654bb13a29fce2085f5c8d2abfe
kean <grebenyuk.alexander@gmail.com>
2020-05-20 09:17:38 -0400

    Update to Nuke 9

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
         .library(name: "FetchImage", targets: ["FetchImage"])
     ],
     dependencies: [
-        .package(url: "https://github.com/kean/Nuke.git", from: "8.0.0")
+        .package(url: "https://github.com/kean/Nuke.git", from: "9.0.0")
     ],
     targets: [
         .target(name: "FetchImage", dependencies: ["Nuke"], path: "Source")
